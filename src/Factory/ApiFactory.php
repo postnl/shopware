5f213ec42cd977e04981a8e9300df3415adc4d68
Daniel Schipper <daniel@memo-ict.nl>
2021-12-13 17:10:25 +0100

    Add Api factory

---
+++ src/Factory/ApiFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace PostNl\Shipments\Factory;
+
+use Firstred\PostNL\Entity\Address;
+use Firstred\PostNL\Entity\Customer;
+use Firstred\PostNL\PostNL;
+
+class ApiFactory
+{
+    public function __construct() {
+        //TODO ConfigService
+    }
+
+    public function createClient(string $apiKey, array $customerData, array $senderAddress, $sandbox = false): PostNL
+    {
+        $customer = Customer::create($customerData);
+        $customer->setAddress((Address::create($senderAddress))->setAddressType('02'));
+
+        return new PostNL($customer, $apiKey, $sandbox);
+    }
+
+    // TODO: createForSalesChannel($salesChannelId, $context);
+}
