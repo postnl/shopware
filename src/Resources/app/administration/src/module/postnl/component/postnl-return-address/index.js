79e28650b379965f6650b59af029852fa1a47e22
Daniel Schipper <daniel@memo-ict.nl>
2021-12-20 17:15:00 +0100

    Add sender address and wip return address

---
+++ src/Resources/app/administration/src/module/postnl/component/postnl-return-address/index.js
@@ -0,0 +1,56 @@
+import template from './postnl-return-address.html.twig';
+
+const { Component, Mixin } = Shopware;
+
+Component.extend('postnl-return-address', 'memo-config', {
+    template,
+
+    inject: [
+        'repositoryFactory'
+    ],
+
+    mixins: [
+        Mixin.getByName('memo-config-access'),
+        Mixin.getByName('memo-grid-span'),
+    ],
+
+    data() {
+        return {
+            country: null,
+        }
+    },
+
+    computed: {
+        countryRepository() {
+            return this.repositoryFactory.create('country');
+        },
+
+        countryId() {
+            return this.getJsonConfigItem('senderAddress')?.country;
+        }
+    },
+
+    watch: {
+        countryId: {
+            handler(value) {
+                if(!!value) {
+                    this.getCountryName(value);
+                    return;
+                }
+
+                this.country = null;
+            },
+            immediate: true,
+        }
+    },
+
+    methods: {
+        getCountryName(countryId) {
+            this.countryRepository
+                .get(countryId, Shopware.Context.api)
+                .then(country => {
+                    this.country = country;
+                });
+        }
+    }
+})
