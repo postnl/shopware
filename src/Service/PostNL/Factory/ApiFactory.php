d52a67f7ed95e06d1c67b7fcc33aa73b0a6ecec1
Daniel Schipper <daniel@memo-ict.nl>
2022-01-25 13:30:20 +0100

    WIP Create client for saleschannel

--- src/Service/PostNL/Factory/ApiFactory.php
+++ src/Service/PostNL/Factory/ApiFactory.php
@@ -5,20 +5,72 @@ namespace PostNL\Shipments\Service\PostNL\Factory;
 use Firstred\PostNL\Entity\Address;
 use Firstred\PostNL\Entity\Customer;
 use Firstred\PostNL\PostNL;
+use PostNL\Shipments\Service\Shopware\ConfigService;
+use Psr\Log\LoggerInterface;
 
 class ApiFactory
 {
-    public function __construct() {
-        //TODO ConfigService
+    /**
+     * @var ConfigService
+     */
+    private $configService;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @param ConfigService $configService
+     * @param LoggerInterface $logger
+     */
+    public function __construct(ConfigService $configService, LoggerInterface $logger)
+    {
+        $this->configService = $configService;
+        $this->logger = $logger;
     }
 
-    public function createClient(string $apiKey, $sandbox = false, array $customerData = [], array $senderAddress = []): PostNL
+    public function createClient(
+        string $apiKey,
+        bool $sandbox = false,
+        array $customerData = [],
+        array $senderAddress = []
+    ): PostNL
     {
+        $this->logger->debug("Creating API client", [
+            'apiKey' => sprintf("%s..%s", substr($apiKey, 0, 6), substr($apiKey, -6)),
+            'sandbox' => $sandbox,
+            'customerData' => $customerData,
+            'senderAddress' => $senderAddress,
+        ]);
+
         $customer = Customer::create($customerData);
         $customer->setAddress((Address::create($senderAddress))->setAddressType('02'));
 
         return new PostNL($customer, $apiKey, $sandbox);
     }
 
-    // TODO: createForSalesChannel($salesChannelId, $context);
+    /**
+     * @param string $salesChannelId
+     * @return PostNL
+     */
+    public function createClientForSalesChannel(string $salesChannelId): PostNL
+    {
+        $this->logger->debug("Creating API client for saleschannel", [
+            'salesChannelId' => $salesChannelId
+        ]);
+
+        $config = $this->configService->getConfiguration($salesChannelId);
+
+        $client = $this->createClient(
+            $config->isSandboxMode()
+                ? $config->getSandboxApiKey()
+                : $config->getProductionApiKey(),
+            $config->isSandboxMode(),
+            $config->getCustomerData()->getVarsForApi()
+        );
+
+        dd($config, $client);
+    }
+
 }
