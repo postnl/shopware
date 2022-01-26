233bd762d1aa9092e3ba4e969ca0cbb328dd0b55
Daniel Schipper <daniel@memo-ict.nl>
2022-01-26 16:10:58 +0100

    Remove one dir level

--- src/Component/Migration/ProductMigration.php
+++ src/Component/Migration/ProductMigration.php
@@ -5,8 +5,8 @@ namespace PostNL\Shipments\Component\Migration;
 use Doctrine\DBAL\Connection;
 use PostNL\Shipments\Entity\ProductCode\Aggregate\ProductCodeConfigTranslation\ProductCodeConfigTranslationDefinition;
 use PostNL\Shipments\Entity\ProductCode\ProductCodeConfigDefinition;
-use PostNL\Shipments\Service\PostNL\Product\Code\DeliveryType;
-use PostNL\Shipments\Service\PostNL\Product\Code\Zone\Zone;
+use PostNL\Shipments\Service\PostNL\Product\DeliveryType;
+use PostNL\Shipments\Service\PostNL\Product\Zone\Zone;
 use Shopware\Core\Framework\Migration\MigrationStep;
 
 abstract class ProductMigration extends MigrationStep
--- src/Service/PostNL/Product/Zone/Zone.php
+++ src/Service/PostNL/Product/Zone/Zone.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace PostNL\Shipments\Service\PostNL\Product\Code\Zone;
+namespace PostNL\Shipments\Service\PostNL\Product\Zone;
 
 interface Zone
 {
