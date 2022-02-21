dbc4a174340ce8907e4fa0db5db384e425e12382
Daniel Schipper <daniel@memo-ict.nl>
2022-02-21 14:52:44 +0100

    Move config components

--- src/Resources/app/administration/src/module/postnl/component/index.js
+++ src/Resources/app/administration/src/module/postnl/component/index.js
@@ -1,5 +1,2 @@
-import './postnl-config-api-credentials-test';
-import './postnl-config-customer-data';
-import './postnl-config-sender-address';
-import './postnl-config-return-address';
+import './config';
 import './postnl-product-selection';
