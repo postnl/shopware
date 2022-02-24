0c48a9e6b22d40b54a33105236b9db1ea5baa044
Daniel Schipper <daniel@memo-ict.nl>
2022-02-24 15:24:28 +0100

    WIP commit

--- src/Subscriber/CheckoutSubscriber.php
+++ src/Subscriber/CheckoutSubscriber.php
@@ -2,9 +2,12 @@
 
 namespace PostNL\Shipments\Subscriber;
 
+use Firstred\PostNL\Entity\Location;
+use Firstred\PostNL\Entity\Request\GetNearestLocations;
 use PostNL\Shipments\Service\Attribute\Factory\AttributeFactory;
 use PostNL\Shipments\Service\PostNL\Factory\ApiFactory;
 use PostNL\Shipments\Struct\ShippingMethodStruct;
+use Shopware\Core\Framework\Struct\ArrayStruct;
 use Shopware\Storefront\Page\Checkout\Confirm\CheckoutConfirmPageLoadedEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
@@ -51,26 +54,44 @@ class CheckoutSubscriber implements EventSubscriberInterface
 
         switch ($attributes->getDeliveryType()) {
             case 'shipment':
-
+                $this->handleShipment($event);
                 break;
             case 'pickup':
                 $this->handlePickup($event);
                 break;
             case 'mailbox':
-
+                $this->handleMailbox($event);
                 break;
         }
     }
 
-    protected function handlePickup(CheckoutConfirmPageLoadedEvent $event)
+    protected function handleShipment(CheckoutConfirmPageLoadedEvent $event): void
+    {
+    }
+
+    protected function handlePickup(CheckoutConfirmPageLoadedEvent $event): void
     {
         $apiClient = $this->apiFactory->createClientForSalesChannel(
             $event->getSalesChannelContext()->getSalesChannelId(),
             $event->getContext()
         );
 
-        dd($apiClient);
+        $address = $event->getPage()->getCart()->getDeliveries()->first()->getLocation()->getAddress();
 
+        $locationRequest = new GetNearestLocations($address->getCountry()->getIso(), new Location($address->getZipcode()));
+        $locationResponse = $apiClient->getNearestLocations($locationRequest);
+
+        $struct = new ArrayStruct();
+        foreach($locationResponse->getGetLocationsResult()->getResponseLocation() as $responseLocation) {
+            $struct->set($responseLocation->getId(), $responseLocation);
+        }
+
+        $event->getSalesChannelContext()->getShippingMethod()->addExtension('postnl_pickup', $struct);
     }
+
+    protected function handleMailbox(CheckoutConfirmPageLoadedEvent $event): void
+    {
+    }
+
 
 }
