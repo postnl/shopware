48f0684c94040a39593419a6c0c3498b894d18cd
Daniel Schipper <daniel@memo-ict.nl>
2022-03-22 14:31:49 +0100

    Manually create shipping method service during activate

--- src/PostNL.php
+++ src/PostNL.php
@@ -3,6 +3,7 @@
 namespace PostNL\Shopware6;
 
 use PostNL\Shopware6\Service\Shopware\ShippingMethodService;
+use Shopware\Core\Content\Media\MediaService;
 use Shopware\Core\Framework\Plugin;
 use Shopware\Core\Framework\Plugin\Context\ActivateContext;
 use Shopware\Core\Framework\Plugin\Context\UninstallContext;
@@ -20,7 +21,14 @@ class PostNL extends Plugin
     public function activate(ActivateContext $activateContext): void
     {
         /** @var ShippingMethodService $shippingMethodService */
-        $shippingMethodService = $this->container->get(ShippingMethodService::class);
+        $shippingMethodService = new ShippingMethodService(
+            $this->container->get('delivery_time.repository'),
+            $this->container->get('media.repository'),
+            $this->container->get('rule.repository'),
+            $this->container->get('shipping_method.repository'),
+            $this->container->get(MediaService::class),
+            $this->container->get('postnl.logger')
+        );
         $shippingMethodService->createShippingMethods($this->getPath(), $activateContext->getContext());
     }
 
