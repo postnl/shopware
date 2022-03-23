43200107511c73605047f873cabcf8959bb4edc9
Spencer Shaw <spencer@memo-ict.nl>
2022-03-23 11:32:21 +0100

    wip: add extra fields for query result, fix field requirements

--- src/Resources/app/storefront/src/plugin/postnl-postal-code-check.plugin.js
+++ src/Resources/app/storefront/src/plugin/postnl-postal-code-check.plugin.js
@@ -1,21 +1,61 @@
 import Plugin from 'src/plugin-system/plugin.class';
-import DomAccess from 'src/helper/dom-access.helper';
 import HttpClient from 'src/service/http-client.service';
+import DomAccess from 'src/helper/dom-access.helper';
+import PluginManager from 'src/plugin-system/plugin.manager';
 
-export default class PostnlPostalCodeCheckPlugin extends Plugin{
+
+export default class PostnlPostalCodeCheckPlugin extends Plugin {
     static options = {
         url: window.router['frontend.address.postnl.postal-code-check'],
         csrfToken: 'put token here',
-        countries:[]
-    }
-    init() {
-        this._client = new HttpClient();
-        console.log(this.options);
-        this._checkPostalCode();
-        // this._registerEvents();
+        countries: []
     }
 
-    _checkPostalCode(){
+    init() {
+        this._client = new HttpClient();
+        // this._checkPostalCode();
+        this._registerEvents();
+        this._updateRequired();
+    }
+
+    _registerEvents() {
+        //Checkbox remove required
+        const jsToggle = document.querySelector('#differentShippingAddress');
+        const jsToggleInstance = PluginManager.getPluginInstanceFromElement(jsToggle, 'FormFieldToggle');
+        jsToggleInstance.$emitter.subscribe('onChange', () => {
+            this._updateRequired();
+        })
+        //Country checker
+        DomAccess.querySelectorAll(this.el, '.country-select').forEach(input => {
+            input.addEventListener('change', this.onChangeSelectedCountry.bind(this));
+        });
+    }
+
+    _updateRequired(){
+        //Check which form is hidden
+        const postElement = DomAccess.querySelector(this.el, '#postNLAddressRow');
+        const postHidden = postElement.getAttribute('hidden');
+        const defaultElement = DomAccess.querySelector(this.el, '#defaultAddressRow');
+        const defaultHidden = defaultElement.getAttribute('hidden');
+
+        if (postHidden==='hidden'){
+            this._swapRequired(defaultElement,postElement)
+        }
+        if (defaultHidden==='hidden'){
+            this._swapRequired(postElement,defaultElement)
+        }
+    }
+
+    _swapRequired(required,notRequired){
+        DomAccess.querySelectorAll(notRequired, 'input,select').forEach(input => {
+            input.removeAttribute('required');
+        });
+        DomAccess.querySelectorAll(required, 'input,select').forEach(input => {
+            input.setAttribute('required','required');
+        });
+    }
+
+    _checkPostalCode() {
         const data = this._getRequestData();
         data['postalCode'] = '7821 AC';
         data['houseNumber'] = '20';
@@ -30,11 +70,32 @@ export default class PostnlPostalCodeCheckPlugin extends Plugin{
 
     _getRequestData() {
         const data = {};
-
         if (window.csrf.enabled && window.csrf.mode === 'twig') {
             data['_csrf_token'] = this.options.csrfToken;
         }
-
         return data;
+    }
+
+    _adaptFormToSelectedCountryId(countryId) {
+        if (this.options.countries[countryId] != null) {
+            this._showCustomForm(true);
+        } else {
+            this._showCustomForm(false);
+        }
+    }
+
+    _showCustomForm(show){
+        if (show){
+            DomAccess.querySelector(this.el, '#postNLAddressRow').removeAttribute('hidden');
+            DomAccess.querySelector(this.el, '#defaultAddressRow').setAttribute('hidden','hidden');
+        }else{
+            DomAccess.querySelector(this.el, '#postNLAddressRow').setAttribute('hidden','hidden');
+            DomAccess.querySelector(this.el, '#defaultAddressRow').removeAttribute('hidden');
+        }
+        this._updateRequired();
+    }
+
+    onChangeSelectedCountry(e) {
+        this._adaptFormToSelectedCountryId(e.target.value)
     }
 }
