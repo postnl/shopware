c320c0aea82a3c672f0be19e508ab629042eb22d
Daniel Schipper <daniel@memo-ict.nl>
2022-03-23 12:20:31 +0100

    POST-57 Add separate component for API link

--- src/Resources/app/administration/src/module/postnl/component/config/index.js
+++ src/Resources/app/administration/src/module/postnl/component/config/index.js
@@ -1,5 +1,6 @@
 import './product-selection';
 
+import './postnl-config-api-credentials-link';
 import './postnl-config-api-credentials-test';
 import './postnl-config-customer-data';
 import './postnl-config-sender-address';
---
+++ src/Resources/app/administration/src/module/postnl/component/config/postnl-config-api-credentials-link/index.js
@@ -0,0 +1,7 @@
+import template from "./postnl-config-api-credentials-link.html.twig";
+
+const { Component} = Shopware;
+
+Component.register('postnl-config-api-credentials-link', {
+    template,
+});
