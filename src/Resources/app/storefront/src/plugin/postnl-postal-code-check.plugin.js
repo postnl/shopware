3af737934a368df76f5c493b23ca84a997074234
Spencer Shaw <spencer@memo-ict.nl>
2022-03-24 13:57:28 +0100

    add: data spinner

--- src/Resources/app/storefront/src/plugin/postnl-postal-code-check.plugin.js
+++ src/Resources/app/storefront/src/plugin/postnl-postal-code-check.plugin.js
@@ -2,21 +2,30 @@ import Plugin from 'src/plugin-system/plugin.class';
 import HttpClient from 'src/service/http-client.service';
 import DomAccess from 'src/helper/dom-access.helper';
 import PluginManager from 'src/plugin-system/plugin.manager';
-
+import Debouncer from 'src/helper/debouncer.helper'
+import ElementLoadingIndicatorUtil from 'src/utility/loading-indicator/element-loading-indicator.util'
 
 export default class PostnlPostalCodeCheckPlugin extends Plugin {
     static options = {
         url: window.router['frontend.address.postnl.postal-code-check'],
         csrfToken: 'put token here',
-        countries: []
+        countries: [],
+        concatPrefix: ""
     }
+
 
     init() {
         this._client = new HttpClient();
-        // this._checkPostalCode();
+
+        //Register elements
+        this.zipcode= DomAccess.querySelector(this.el, '#' + this.options.concatPrefix + 'PostNLAddressZipcode');
+        this.houseNumber = DomAccess.querySelector(this.el, '#' + this.options.concatPrefix + 'PostNLAddressHouseNumber');
+        this.houseNumberAddition = DomAccess.querySelector(this.el, '#' + this.options.concatPrefix + 'PostNLAddressHouseNumberAddition');
+
         this._registerEvents();
         this._updateRequired();
         this._setupLinkedFields();
+        console.log(this.options);
     }
 
     _registerEvents() {
@@ -30,43 +39,61 @@ export default class PostnlPostalCodeCheckPlugin extends Plugin {
         DomAccess.querySelectorAll(this.el, '.country-select').forEach(input => {
             input.addEventListener('change', this.onChangeSelectedCountry.bind(this));
         });
+        const debouceLookup = Debouncer.debounce(this._lookupAddress.bind(this), 2000);
+
+        //Address fields
+        this.zipcode.addEventListener('keyup', debouceLookup)
+        this.houseNumber.addEventListener('keyup', debouceLookup)
+        this.houseNumberAddition.addEventListener('keyup', debouceLookup)
     }
 
-    _updateRequired(){
+    _lookupAddress() {
+        //Are all fields filled?
+        if (this.zipcode.value != null && this.houseNumber.value != null) {
+            this._checkPostalCode(this.zipcode.value, this.houseNumber.value, this.houseNumberAddition.value);
+        }
+    }
+
+    _updateRequired() {
         //Check which form is hidden
-        const postElement = DomAccess.querySelector(this.el, '#postNLAddressRow');
-        const postHidden = postElement.getAttribute('hidden');
-        const defaultElement = DomAccess.querySelector(this.el, '#defaultAddressRow');
-        const defaultHidden = defaultElement.getAttribute('hidden');
+        let postElement = DomAccess.querySelector(this.el, '#postNLAddressRow');
+        let postHidden = postElement.getAttribute('hidden');
+        let defaultElement = DomAccess.querySelector(this.el, '#defaultAddressRow');
+        let defaultHidden = defaultElement.getAttribute('hidden');
 
-        if (postHidden==='hidden'){
-            this._swapRequired(defaultElement,postElement)
+        if (postHidden === 'hidden') {
+            this._swapRequired(defaultElement, postElement)
         }
-        if (defaultHidden==='hidden'){
-            this._swapRequired(postElement,defaultElement)
+        if (defaultHidden === 'hidden') {
+            this._swapRequired(postElement, defaultElement)
         }
     }
 
-    _swapRequired(required,notRequired){
+    _swapRequired(required, notRequired) {
         DomAccess.querySelectorAll(notRequired, 'input,select').forEach(input => {
             input.removeAttribute('required');
         });
         DomAccess.querySelectorAll(required, 'input,select').forEach(input => {
-            input.setAttribute('required','required');
+            input.setAttribute('required', 'required');
         });
     }
 
-    _checkPostalCode() {
+    _checkPostalCode(zipcodeValue, houseNumberValue, houseNumberAdditionValue) {
         const data = this._getRequestData();
         data['postalCode'] = '7821 AC';
         data['houseNumber'] = '20';
         // data['houseNumberAddition'] = 'NL';
-        this._client.post(this.options.url, JSON.stringify(data), content => this._parseRequest(JSON.parse(content)));
+        ElementLoadingIndicatorUtil.create(this.el);
+        this._client.post(this.options.url, JSON.stringify(data), content =>{
+            ElementLoadingIndicatorUtil.remove(this.el);
+            this._parseRequest(JSON.parse(content))
+        });
     }
 
     _parseRequest(data) {
         console.log('Logging data')
         console.log(data)
+        //TODO: fill in all the data you got here
     }
 
     _getRequestData() {
@@ -85,38 +112,48 @@ export default class PostnlPostalCodeCheckPlugin extends Plugin {
         }
     }
 
-    _showCustomForm(show){
-        if (show){
+    _showCustomForm(show) {
+        if (show) {
             DomAccess.querySelector(this.el, '#postNLAddressRow').removeAttribute('hidden');
-            DomAccess.querySelector(this.el, '#defaultAddressRow').setAttribute('hidden','hidden');
-        }else{
-            DomAccess.querySelector(this.el, '#postNLAddressRow').setAttribute('hidden','hidden');
+            DomAccess.querySelector(this.el, '#defaultAddressRow').setAttribute('hidden', 'hidden');
+        } else {
+            DomAccess.querySelector(this.el, '#postNLAddressRow').setAttribute('hidden', 'hidden');
             DomAccess.querySelector(this.el, '#defaultAddressRow').removeAttribute('hidden');
         }
         this._updateRequired();
     }
 
-    _setupLinkedFields(){
+    _setupLinkedFields() {
         //Postal code
-        this._linkFields('#billingAddressAddressZipcode','#billingAddressPostNLAddressZipcode')
-        this._linkFields('#shippingAddressAddressZipcode','#shippingAddressPostNLAddressZipcode')
-        //Address
-        //TODO: fix this with converter because the address needs to be added
-        this._linkFields('#billingAddressAddressStreet','#shippingAddressPostNLAddressStreet')
-        this._linkFields('#shippingAddressAddressStreet','#shippingAddressPostNLAddressStreet')
+        this._linkFields('#' + this.options.concatPrefix + 'AddressZipcode',
+            '#' + this.options.concatPrefix + 'PostNLAddressZipcode')
+
+        this._linkFields('#' + this.options.concatPrefix + 'PostNLAddressZipcode',
+            '#' + this.options.concatPrefix + 'AddressZipcode')
+
         //City
-        this._linkFields('#billingAddressAddressCity','#shippingAddressPostNLAddressCity')
-        this._linkFields('#shippingAddressAddressCity','#shippingAddressPostNLAddressCity')
+        this._linkFields('#' + this.options.concatPrefix + 'AddressCity',
+            '#' + this.options.concatPrefix + 'PostNLAddressCity')
+        this._linkFields('#' + this.options.concatPrefix + 'PostNLAddressCity',
+            '#' + this.options.concatPrefix + 'AddressCity')
+
+        //Address (street + house number in shopware)
+        //TODO: fix this with converter because the address needs to be compounded
+        // this._linkFields('#'+this.options.concatPrefix+'AddressStreet',
+        //     '#'+this.options.concatPrefix+'PostNLAddressStreet')
+        // this._linkFields('#'+this.options.concatPrefix+'PostNLAddressStreet',
+        //     '#'+this.options.concatPrefix+'AddressStreet')
 
     }
 
-    _linkFields(field1Selector,field2Selector){
+    _linkFields(field1Selector, field2Selector) {
+        console.log('Linking ', field1Selector, 'with', field2Selector);
         const field1 = document.querySelector(field1Selector);
         const field2 = document.querySelector(field2Selector);
-        field1.addEventListener('change', this.copyValue.bind(this,field1,field2));
+        field1.addEventListener('change', this.copyValue.bind(this, field1, field2));
     }
 
-    copyValue(sender,receiver){
+    copyValue(sender, receiver) {
         receiver.value = sender.value
     }
 
