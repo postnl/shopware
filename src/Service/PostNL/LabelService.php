c968d9bf03555736a707afc5b83b36006531f4ce
Spencer Shaw <spencer@memo-ict.nl>
2022-04-28 16:23:40 +0200

    gitflow-feature-stash: better_label_generation

--- src/Service/PostNL/LabelService.php
+++ src/Service/PostNL/LabelService.php
@@ -2,12 +2,14 @@
 
 namespace PostNL\Shopware6\Service\PostNL;
 
+use Exception;
 use Firstred\PostNL\Entity\Label;
 use Firstred\PostNL\Entity\Response\GenerateLabelResponse;
 use Firstred\PostNL\Exception\PostNLException;
 use Firstred\PostNL\PostNL;
 use Firstred\PostNL\Util\RFPdi;
 use Firstred\PostNL\Util\Util;
+use PostNL\Shopware6\Service\PostNL\Label\A6OnA4LandscapeLabelConfiguration;
 use setasign\Fpdi\PdfParser\StreamReader;
 
 class LabelService
@@ -77,7 +79,7 @@ class LabelService
                 $sizes = Util::getPdfSizeAndOrientation($pdfContent);
 
                 if ('A6' === $sizes['iso']) {
-                    foreach($responseShipment->getResponseShipments()[0]->getLabels() as $label) {
+                    foreach ($responseShipment->getResponseShipments()[0]->getLabels() as $label) {
                         $pdfContent = base64_decode($label->getContent());
 
                         if ($firstPage) {
@@ -156,4 +158,100 @@ class LabelService
         return base64_encode($pdf->output('', 'S'));
     }
 
+    const ORIENTATION_FORMAT_PORTRAIT = 'P';
+    const ORIENTATION_FORMAT_LANDSCAPE = 'L';
+    const LABEL_FORMAT_A4 = 'A4';
+    const LABEL_FORMAT_A6 = 'A6';
+
+    /**
+     * @param array $responseShipments
+     * @param A6OnA4LandscapeLabelConfiguration[] $labelConfigurations
+     * @param string $mergedFormat The format the labels will be returned as
+     * @return string
+     * @throws \setasign\Fpdi\PdfParser\CrossReference\CrossReferenceException
+     * @throws \setasign\Fpdi\PdfParser\Filter\FilterException
+     * @throws \setasign\Fpdi\PdfParser\PdfParserException
+     * @throws \setasign\Fpdi\PdfParser\Type\PdfTypeException
+     * @throws \setasign\Fpdi\PdfReader\PdfReaderException
+     */
+    public function newMerge(
+        array  $responseShipments,
+        array  $labelConfigurations = [],
+        string $mergedFormat = self::LABEL_FORMAT_A4
+
+    ): string
+    {
+        //Collect all labels and sort them
+        $labels = [];
+        foreach ($responseShipments as $responseShipment) {
+            foreach ($responseShipment->getResponseShipments()[0]->getLabels() as $label) {
+                $labels[] = $label;
+            }
+        }
+        //Sort the labels by size, big ones first
+        usort($labels, array($this, 'labelSizeSort'));
+
+        //Start a new pdf
+        // Disable header and footer
+        $pdf = new RFPdi(self::ORIENTATION_FORMAT_LANDSCAPE, 'mm', $mergedFormat === self::LABEL_FORMAT_A4 ? [210, 297] : [105, 148]);
+
+        foreach ($labels as $label) {
+            $pdfContent = base64_decode($label->getContent());
+            $sizeAndOrientation = Util::getPdfSizeAndOrientation($pdfContent);
+            if ($sizeAndOrientation['iso'] == $mergedFormat) {
+                //Its 1:1 just add it
+                $pdf->addPage($sizeAndOrientation['orientation']);
+                //Import the page
+                $pdf->setSourceFile(StreamReader::createByString($pdfContent));
+                $pdf->useTemplate($pdf->importPage(1));
+            } else {
+                if ($sizeAndOrientation['iso'] == self::LABEL_FORMAT_A4 && $mergedFormat == self::LABEL_FORMAT_A6) {
+                    //No scaling implemented
+                    throw new Exception('Destination size is smaller than origin');
+                }
+                //If a6 on an a4 result, it will be a 2x2
+                // Add a config if we do not have any
+                if (count($labelConfigurations) == 0) {
+                    $labelConfigurations[] = A6OnA4LandscapeLabelConfiguration::createFullLabel();
+                }
+                $labelConfiguration = $labelConfigurations[0];
+                //Start a new page if needed
+                if (!$labelConfiguration->active) {
+                    $pdf->addPage();
+                    $labelConfiguration->active = true;
+                }
+                //Get a slot
+                $freeSlot = $labelConfiguration->getFreeSlot();
+                $pdf->setSourceFile(StreamReader::createByString($pdfContent));
+                //Put a label in it (rotate if needed)
+                //TODO:rotate?
+                $importedPage = $pdf->importPage(1);
+                $coordinates = A6OnA4LandscapeLabelConfiguration::getCoordinatesXYForSlot($freeSlot);
+                $pdf->useImportedPage($importedPage, $coordinates[0], $coordinates[1]);
+                //Mark slot as filled
+                $labelConfiguration->fillSlot($freeSlot);
+                //Discard if full
+                if (!$labelConfiguration->hasFreeSlots()) {
+                    array_shift($labelConfigurations);
+                }
+            }
+        }
+
+        return base64_encode($pdf->output('', 'S'));
+    }
+
+    private function labelSizeSort($a, $b)
+    {
+        $aSize = Util::getPdfSizeAndOrientation($a);
+        $bSize = Util::getPdfSizeAndOrientation($b);
+        if ($aSize === $bSize) {
+            return 0;
+        }
+        if ($aSize == 'A6' && $bSize == 'A4') {
+            return 1;
+        }
+        if ($aSize == 'A4' && $bSize == 'A6') {
+            return -1;
+        }
+    }
 }
