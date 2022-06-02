0de4493fe559e44757d3c7cf58e621371ede692b
Spencer Shaw <spencer@memo-ict.nl>
2022-06-02 12:58:27 +0200

    add: info above settings with translations

--- src/Resources/app/administration/src/module/postnl/component/config/index.js
+++ src/Resources/app/administration/src/module/postnl/component/config/index.js
@@ -5,3 +5,4 @@ import './postnl-config-api-credentials-test';
 import './postnl-config-customer-data';
 import './postnl-config-sender-address';
 import './postnl-config-return-address';
+import './postnl-config-info'
