15551984945258f73cbf8946d984ed1ccade5d6c
Daniel Schipper <daniel@memo-ict.nl>
2022-06-13 13:47:00 +0200

    label extractor services

--- src/Service/PostNL/Label/Extractor/SendShipmentsLabelExtractor.php
+++ src/Service/PostNL/Label/Extractor/SendShipmentsLabelExtractor.php
@@ -5,15 +5,24 @@ namespace PostNL\Shopware6\Service\PostNL\Label\Extractor;
 use Firstred\PostNL\Entity\Response\GenerateLabelResponse;
 use Firstred\PostNL\Entity\Response\SendShipmentResponse;
 use Firstred\PostNL\Exception\NotImplementedException;
+use PostNL\Shopware6\Service\PostNL\Label\Label;
 
 class SendShipmentsLabelExtractor implements LabelExtractorInterface
 {
     /**
-     * @inheritDoc
+     * @param SendShipmentResponse $response
+     * @return Label[]
      */
-    public function extract(array $response): array
+    public function extract($response): array
     {
-        // TODO: Implement extract() method.
-        throw new NotImplementedException();
+        $labels = [];
+
+        foreach($response->getResponseShipments() as $shipment) {
+            foreach ($shipment->getLabels() as $label) {
+                $labels[] = new Label($label->getContent(), $shipment->getBarcode(), $label->getLabeltype());
+            }
+        }
+
+        return $labels;
     }
 }
