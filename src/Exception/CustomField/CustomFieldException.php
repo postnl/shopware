8659ce5cd2a21beb86b525363f5e936473ccfdeb
Daniel Schipper <daniel@memo-ict.nl>
2022-06-14 12:40:05 +0200

    Fix stan

--- src/Exception/CustomField/CustomFieldException.php
+++ src/Exception/CustomField/CustomFieldException.php
@@ -7,10 +7,15 @@ use Shopware\Core\Framework\ShopwareException;
 abstract class CustomFieldException extends \RuntimeException implements ShopwareException
 {
     /**
-     * @var array
+     * @var array<string, mixed>
      */
     protected $parameters = [];
 
+    /**
+     * @param string               $message
+     * @param array<string, mixed> $parameters
+     * @param \Throwable|null      $previous
+     */
     public function __construct(string $message, array $parameters = [], ?\Throwable $previous = null)
     {
         $this->parameters = $parameters;
@@ -19,16 +24,27 @@ abstract class CustomFieldException extends \RuntimeException implements Shopwar
         parent::__construct($message, 0, $previous);
     }
 
+    /**
+     * @param bool $withTrace
+     * @return \Generator<mixed>
+     */
     public function getErrors(bool $withTrace = false): \Generator
     {
         yield $this->getCommonErrorData($withTrace);
     }
 
+    /**
+     * @return mixed[]
+     */
     public function getParameters(): array
     {
         return $this->parameters;
     }
 
+    /**
+     * @param bool $withTrace
+     * @return array<string, mixed>
+     */
     protected function getCommonErrorData(bool $withTrace = false): array
     {
         $error = [
@@ -46,6 +62,11 @@ abstract class CustomFieldException extends \RuntimeException implements Shopwar
         return $error;
     }
 
+    /**
+     * @param string               $message
+     * @param array<string, mixed> $parameters
+     * @return string
+     */
     protected function parse(string $message, array $parameters = []): string
     {
         $regex = [];
@@ -58,6 +79,6 @@ abstract class CustomFieldException extends \RuntimeException implements Shopwar
             $regex[sprintf('/\{\{(\s+)?(%s)(\s+)?\}\}/', $key)] = $value;
         }
 
-        return preg_replace(array_keys($regex), array_values($regex), $message);
+        return (string)preg_replace(array_keys($regex), array_values($regex), $message);
     }
 }
