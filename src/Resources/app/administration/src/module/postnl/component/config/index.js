b7ddd1d0d4f4f56e977ec4ad3dbf10ee9d2cdd04
Spencer Shaw <spencer@memo-ict.nl>
2022-06-15 15:27:21 +0200

    add: titles and changed descriptions

--- src/Resources/app/administration/src/module/postnl/component/config/index.js
+++ src/Resources/app/administration/src/module/postnl/component/config/index.js
@@ -6,4 +6,5 @@ import './postnl-config-api-credentials-test';
 import './postnl-config-customer-data';
 import './postnl-config-sender-address';
 import './postnl-config-return-address';
-import './postnl-config-info'
+import './postnl-config-info';
+import './postnl-config-description-text';
