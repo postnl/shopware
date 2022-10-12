3e8531680cb534cc8bf9b5e72be3e14a7f7da212
Spencer Shaw <spencer@memo-ict.nl>
2022-10-12 16:12:01 +0200

    add: more flexibility and shipping tests

--- tests/Cypress/cypress/e2e/address-check/address-check.cy.js
+++ tests/Cypress/cypress/e2e/address-check/address-check.cy.js
@@ -10,45 +10,41 @@ function checkDutch() {
     cy.get('[id*=postNLAddressRow-]').should('be.visible')
 }
 
-function checkDisabledFields() {
-    cy.switchCountry('Netherlands')
+function checkDisabledFields(type) {
 
-    cy.get('[id^=billingAddressPostNLAddressStreet-]').should('be.disabled')
-    cy.get('[id^=billingAddressPostNLAddressCity-]').should('be.disabled')
+    cy.get('[id^='+type+'AddressPostNLAddressStreet-]').should('be.disabled')
+    cy.get('[id^='+type+'AddressPostNLAddressCity-]').should('be.disabled')
 }
 
-function lookUpAddress() {
-    cy.switchCountry('Netherlands')
-
+function lookUpAddress(type) {
     cy.fixture('user').then((user) => {
-        cy.get('[id^=billingAddressPostNLAddressZipcode-]').type(user.zipcode);
-        cy.get('[id^=billingAddressPostNLAddressHouseNumber-]').type(user.housenumber);
+        cy.get('[id^='+type+'AddressPostNLAddressZipcode-]').type(user.zipcode);
+        cy.get('[id^='+type+'AddressPostNLAddressHouseNumber-]').type(user.housenumber);
     })
 
     cy.intercept('POST', '/widget/address/postnl/postalcode-check').as('postalcode-check')
 
     cy.wait('@postalcode-check').its('response.statusCode').should('eq', 200)
 
-    cy.get('[id^=billingAddressPostNLAddressStreet-]').should('have.value', 'Leidsestraatweg')
-    cy.get('[id^=billingAddressPostNLAddressCity-]').should('have.value', 'WOERDEN')
-    cy.get('[id^=billingAddressPostNLAddressHouseNumberAdditionDatalist-] > option').should('have.length', 13)
+    cy.get('[id^='+type+'AddressPostNLAddressStreet-]').should('have.value', 'Leidsestraatweg')
+    cy.get('[id^='+type+'AddressPostNLAddressCity-]').should('have.value', 'WOERDEN')
+    cy.get('[id^='+type+'AddressPostNLAddressHouseNumberAdditionDatalist-] > option').should('have.length', 13)
 }
 
-function lookUpAddressError() {
-    cy.switchCountry('Netherlands')
+function lookUpAddressError(type) {
 
     cy.fixture('user').then((user) => {
-        cy.get('[id^=billingAddressPostNLAddressZipcode-]').type(user.zipcode);
-        cy.get('[id^=billingAddressPostNLAddressHouseNumber-]').type('5');
+        cy.get('[id^='+type+'AddressPostNLAddressZipcode-]').type(user.zipcode);
+        cy.get('[id^='+type+'AddressPostNLAddressHouseNumber-]').type('5');
     })
 
     cy.intercept('POST', '/widget/address/postnl/postalcode-check').as('postalcode-check')
 
     cy.wait('@postalcode-check').its('response.statusCode').should('eq', 200)
 
-    cy.get('[id^=billingAddresspostNLAddressRow-]').contains('No address has been found, check if your input is correct.')
-    cy.get('[id^=billingAddressPostNLAddressStreet-]').should('not.be.disabled')
-    cy.get('[id^=billingAddressPostNLAddressCity-]').should('not.be.disabled')
+    cy.get('[id^='+type+'AddresspostNLAddressRow-]').contains('No address has been found, check if your input is correct.')
+    cy.get('[id^='+type+'AddressPostNLAddressStreet-]').should('not.be.disabled')
+    cy.get('[id^='+type+'AddressPostNLAddressCity-]').should('not.be.disabled')
 }
 
 describe('Register page check', () => {
@@ -65,19 +61,26 @@ describe('Register page check', () => {
     })
 
     it('Should have disabled fields on Dutch addresses', () => {
-        checkDisabledFields();
+        cy.switchCountry('Netherlands')
+        checkDisabledFields('billing');
     })
 
     it('Should look up an address', () => {
-        lookUpAddress();
+        cy.switchCountry('Netherlands')
+        lookUpAddress('billing');
     })
 
     it('Should show an error on invalid address and unlock the fields', () => {
-        lookUpAddressError();
+        cy.switchCountry('Netherlands')
+        lookUpAddressError('billing');
     })
 })
 
 describe('Checkout page checks', () => {
+
+    function switchShippingCountry(country) {
+        cy.get('#shippingAddressAddressCountry').select(country)
+    }
 
     beforeEach(() => {
         cy.checkOutProduct()
@@ -92,16 +95,49 @@ describe('Checkout page checks', () => {
     })
 
     it('Should have disabled fields on Dutch addresses', () => {
-        checkDisabledFields();
+        cy.switchCountry('Netherlands')
+        checkDisabledFields('billing');
+
+        cy.get('#differentShippingAddress').check({force: true});
+        switchShippingCountry('Netherlands');
+        checkDisabledFields('shipping');
     })
 
-    it('Should look up an address', () => {
-        lookUpAddress();
+    it.only('Should look up an address', () => {
+        cy.switchCountry('Netherlands')
+        lookUpAddress('billing');
+
+        cy.get('#differentShippingAddress').check({force: true});
+        switchShippingCountry('Netherlands');
+        lookUpAddress('shipping');
     })
 
     it('Should show an error on invalid address and unlock the fields', () => {
-        lookUpAddressError();
+        cy.switchCountry('Netherlands')
+        lookUpAddressError('billing');
+
+        cy.get('#differentShippingAddress').check({force: true});
+        switchShippingCountry('Netherlands');
+        lookUpAddressError('shipping');
     })
+
+    it('should work on shipping address', function () {
+
+        cy.get('[id*=shippingAddresspostNLAddressRow-]').should('be.visible')
+
+        cy.fixture('user').then((user) => {
+            cy.get('[id^=shippingAddressPostNLAddressZipcode-]').type(user.zipcode);
+            cy.get('[id^=shippingAddressPostNLAddressHouseNumber-]').type(user.housenumber);
+        })
+
+        cy.intercept('POST', '/widget/address/postnl/postalcode-check').as('postalcode-check')
+
+        cy.wait('@postalcode-check').its('response.statusCode').should('eq', 200)
+
+        cy.get('[id^=shippingAddressPostNLAddressStreet-]').should('have.value', 'Leidsestraatweg')
+        cy.get('[id^=shippingAddressPostNLAddressCity-]').should('have.value', 'WOERDEN')
+        cy.get('[id^=shippingAddressPostNLAddressHouseNumberAdditionDatalist-] > option').should('have.length', 13)
+    });
 })
 
 describe('Shipping page add address checks', () => {
@@ -147,7 +183,7 @@ describe('Shipping page add address checks', () => {
         cy.get('#billing-address-create-new [id*=ostNLAddressHouseNumberAdditionDatalist-] > option').should('have.length', 13)
     })
 
-    it.only('Should show an error on invalid address and unlock the fields', () => {
+    it('Should show an error on invalid address and unlock the fields', () => {
         cy.switchCountry('Netherlands')
 
         cy.fixture('user').then((user) => {
