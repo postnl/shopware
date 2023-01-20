aba6ff32f4f1b89cd2ff11be0f0e483b2ca81174
Daniel Schipper <daniel@memo-ict.nl>
2023-01-20 14:50:58 +0100

    Update mapping migration

--- src/Migration/Migration1674219692CreateV2_0Mappings.php
+++ src/Migration/Migration1674219692CreateV2_0Mappings.php
@@ -6,6 +6,7 @@ use Doctrine\DBAL\Connection;
 use PostNL\Shopware6\Defaults;
 use PostNL\Shopware6\Entity\Product\Aggregate\ProductOptionOptionalMappingDefinition;
 use Shopware\Core\Framework\Migration\MigrationStep;
+use Shopware\Core\Framework\Uuid\Uuid;
 
 class Migration1674219692CreateV2_0Mappings extends MigrationStep
 {
@@ -16,61 +17,78 @@ class Migration1674219692CreateV2_0Mappings extends MigrationStep
 
     public function update(Connection $connection): void
     {
-        $connection->transactional(function($connection) {
-            $connection->insert(ProductOptionOptionalMappingDefinition::ENTITY_NAME, [
-                [
-                    'product_id' => Defaults::PRODUCT_SHIPPING_NL_NL_3085,
-                    'option_id' => Defaults::OPTION_118_006,
-                ],
-                [
-                    'product_id' => Defaults::PRODUCT_SHIPPING_NL_NL_3087,
-                    'option_id' => Defaults::OPTION_118_006,
-                ],
-                [
-                    'product_id' => Defaults::PRODUCT_SHIPPING_NL_NL_3089,
-                    'option_id' => Defaults::OPTION_118_006,
-                ],
-                [
-                    'product_id' => Defaults::PRODUCT_SHIPPING_NL_NL_3090,
-                    'option_id' => Defaults::OPTION_118_006,
-                ],
-                [
-                    'product_id' => Defaults::PRODUCT_SHIPPING_NL_NL_3094,
-                    'option_id' => Defaults::OPTION_118_006,
-                ],
-                [
-                    'product_id' => Defaults::PRODUCT_SHIPPING_NL_NL_3096,
-                    'option_id' => Defaults::OPTION_118_006,
-                ],
-                [
-                    'product_id' => Defaults::PRODUCT_SHIPPING_NL_NL_3189,
-                    'option_id' => Defaults::OPTION_118_006,
-                ],
-                [
-                    'product_id' => Defaults::PRODUCT_SHIPPING_NL_NL_3385,
-                    'option_id' => Defaults::OPTION_118_006,
-                ],
-                [
-                    'product_id' => Defaults::PRODUCT_SHIPPING_NL_NL_3389,
-                    'option_id' => Defaults::OPTION_118_006,
-                ],
-                [
-                    'product_id' => Defaults::PRODUCT_SHIPPING_NL_NL_3390,
-                    'option_id' => Defaults::OPTION_118_006,
-                ],
-                [
-                    'product_id' => Defaults::PRODUCT_SHIPPING_NL_BE_4912,
-                    'option_id' => Defaults::OPTION_118_006,
-                ],
-                [
-                    'product_id' => Defaults::PRODUCT_SHIPPING_NL_BE_4941,
-                    'option_id' => Defaults::OPTION_118_006,
-                ],
-                [
-                    'product_id' => Defaults::PRODUCT_SHIPPING_NL_BE_4946,
-                    'option_id' => Defaults::OPTION_118_006,
-                ],
-            ]);
+        $this->insert($connection, ProductOptionOptionalMappingDefinition::ENTITY_NAME, [
+            [
+                'product_id' => Defaults::PRODUCT_SHIPPING_NL_NL_3085,
+                'option_id'  => Defaults::OPTION_118_006,
+            ],
+            [
+                'product_id' => Defaults::PRODUCT_SHIPPING_NL_NL_3087,
+                'option_id'  => Defaults::OPTION_118_006,
+            ],
+            [
+                'product_id' => Defaults::PRODUCT_SHIPPING_NL_NL_3089,
+                'option_id'  => Defaults::OPTION_118_006,
+            ],
+            [
+                'product_id' => Defaults::PRODUCT_SHIPPING_NL_NL_3090,
+                'option_id'  => Defaults::OPTION_118_006,
+            ],
+            [
+                'product_id' => Defaults::PRODUCT_SHIPPING_NL_NL_3094,
+                'option_id'  => Defaults::OPTION_118_006,
+            ],
+            [
+                'product_id' => Defaults::PRODUCT_SHIPPING_NL_NL_3096,
+                'option_id'  => Defaults::OPTION_118_006,
+            ],
+            [
+                'product_id' => Defaults::PRODUCT_SHIPPING_NL_NL_3189,
+                'option_id'  => Defaults::OPTION_118_006,
+            ],
+            [
+                'product_id' => Defaults::PRODUCT_SHIPPING_NL_NL_3385,
+                'option_id'  => Defaults::OPTION_118_006,
+            ],
+            [
+                'product_id' => Defaults::PRODUCT_SHIPPING_NL_NL_3389,
+                'option_id'  => Defaults::OPTION_118_006,
+            ],
+            [
+                'product_id' => Defaults::PRODUCT_SHIPPING_NL_NL_3390,
+                'option_id'  => Defaults::OPTION_118_006,
+            ],
+            [
+                'product_id' => Defaults::PRODUCT_SHIPPING_NL_BE_4912,
+                'option_id'  => Defaults::OPTION_118_006,
+            ],
+            [
+                'product_id' => Defaults::PRODUCT_SHIPPING_NL_BE_4914,
+                'option_id'  => Defaults::OPTION_118_006,
+            ],
+            [
+                'product_id' => Defaults::PRODUCT_SHIPPING_NL_BE_4941,
+                'option_id'  => Defaults::OPTION_118_006,
+            ],
+            [
+                'product_id' => Defaults::PRODUCT_SHIPPING_NL_BE_4946,
+                'option_id'  => Defaults::OPTION_118_006,
+            ],
+        ]);
+    }
+
+    protected function insert(Connection $connection, string $table, array $data)
+    {
+        $connection->transactional(function ($connection) use ($table, $data) {
+            $connection->executeStatement('SET foreign_key_checks = 0');
+
+            foreach ($data as $entry) {
+                $connection->insert($table, array_map(function($id) {
+                    return Uuid::fromHexToBytes($id);
+                }, $entry));
+            }
+
+            $connection->executeStatement('SET foreign_key_checks = 1');
         });
     }
 
