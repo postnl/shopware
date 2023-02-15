2197e032878e8fde86db8a989ccfe1a0bfcd8944
Daniel Schipper <daniel@memo-ict.nl>
2023-02-15 13:56:04 +0100

    WIP filtering timeframes

--- src/Struct/TimeframeCollection.php
+++ src/Struct/TimeframeCollection.php
@@ -45,38 +45,12 @@ class TimeframeCollection extends Collection
 
     public function filterByConfig(ConfigStruct $config)
     {
-        $now = new \DateTimeImmutable("now", new \DateTimeZone('Europe/Amsterdam'));
-
-        $timeParts = explode(':', $config->getCutOffTime());
-        $cutoff = $now->setTime(...array_map('intval', $timeParts));
-
         $interval = $config->getTransitTime();
 
-        // If now is later than cutoff, invert will be 1, otherwise 0.
-        if($now->diff($cutoff)->invert) {
-            $interval += 1;
-        }
+        //foreach($this->getElements() as $element) {
+            //dump($element);
+        //}
 
-        $weekdays = [];
-
-        foreach($config->getHandoverDays() as $day) {
-            switch($day) {
-                case 'sunday': $dayOfWeek = 0; break;
-                case 'monday': $dayOfWeek = 1; break;
-                case 'tuesday': $dayOfWeek = 2; break;
-                case 'wednesday': $dayOfWeek = 3; break;
-                case 'thursday': $dayOfWeek = 4; break;
-                case 'friday': $dayOfWeek = 5; break;
-                case 'saturday': $dayOfWeek = 6; break;
-            }
-
-            $weekdays[] = ($dayOfWeek + $interval) % 7;
-        }
-
-        dd($weekdays);
-
-
-
-        dd($now, $cutoff);
+        //dd($interval);
     }
 }
