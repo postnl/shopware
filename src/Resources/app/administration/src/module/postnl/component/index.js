4114193eae4d78fdef4311fc5f5fd41c6e3c13e2
Daniel Schipper <daniel@memo-ict.nl>
2023-02-16 15:15:52 +0100

    Add currency field component

--- src/Resources/app/administration/src/module/postnl/component/index.js
+++ src/Resources/app/administration/src/module/postnl/component/index.js
@@ -4,3 +4,4 @@ import './postnl-button';
 import './postnl-product-selection';
 import './postnl-change-shipping-modal';
 import './postnl-create-shipment-modal';
+import './postnl-currency-field';
---
+++ src/Resources/app/administration/src/module/postnl/component/postnl-currency-field/index.js
@@ -0,0 +1,5 @@
+import template from './postnl-currency-field.html.twig';
+
+Shopware.Component.register('postnl-currency-field', {
+    template,
+});
