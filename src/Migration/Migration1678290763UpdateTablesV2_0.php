bc3901f6f833fff1bb816bfc9e84db9a55300831
Daniel Schipper <daniel@memo-ict.nl>
2023-03-09 14:23:16 +0100

    Rename migration and update more tables

--- src/Migration/Migration1678290763UpdateTablesV2_0.php
+++ src/Migration/Migration1678290763UpdateTablesV2_0.php
@@ -6,7 +6,7 @@ use Doctrine\DBAL\Connection;
 use PostNL\Shopware6\Component\Migration\MigrationExecuteTrait;
 use Shopware\Core\Framework\Migration\MigrationStep;
 
-class Migration1678290763MakeOptionsHidden extends MigrationStep
+class Migration1678290763UpdateTablesV2_0 extends MigrationStep
 {
     use MigrationExecuteTrait;
 
@@ -18,10 +18,16 @@ class Migration1678290763MakeOptionsHidden extends MigrationStep
     public function update(Connection $connection): void
     {
         $sql = <<<SQL
+ALTER TABLE `postnl_product` ADD `replaced_by_id` BINARY(16) NULL DEFAULT NULL AFTER `id`;
+ALTER TABLE `postnl_product` ADD KEY `fk.postnl_product.replaced_by_id` (`replaced_by_id`);
+ALTER TABLE `postnl_product` ADD CONSTRAINT `fk.postnl_product.replaced_by_id` FOREIGN KEY (`replaced_by_id`)
+    REFERENCES `postnl_product`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
 ALTER TABLE `postnl_option` ADD `hidden` BOOLEAN NOT NULL DEFAULT FALSE AFTER `option`;
 UPDATE `postnl_option` SET `hidden` = '1';
 SQL;
 
+        $this->execute($connection, $sql);
     }
 
     public function updateDestructive(Connection $connection): void
