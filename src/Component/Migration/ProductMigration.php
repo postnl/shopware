d890cc30a903796f3ea7239b6c92723577ab671c
Daniel Schipper <daniel@memo-ict.nl>
2023-03-09 14:24:37 +0100

    Update product migration to deprecate products instead of deleting them

--- src/Component/Migration/ProductMigration.php
+++ src/Component/Migration/ProductMigration.php
@@ -9,6 +9,7 @@ use PostNL\Shopware6\Service\PostNL\Delivery\DeliveryType;
 use PostNL\Shopware6\Service\PostNL\Delivery\Zone\Zone;
 use Shopware\Core\Defaults as ShopwareDefaults;
 use Shopware\Core\Framework\Migration\MigrationStep;
+use Shopware\Core\Framework\Uuid\Uuid;
 
 abstract class ProductMigration extends MigrationStep
 {
@@ -16,7 +17,7 @@ abstract class ProductMigration extends MigrationStep
     use MigrationQuoteTrait;
 
     /**
-     * @param Connection $connection
+     * @param Connection   $connection
      * @param array<mixed> $products
      * @return void
      * @throws \Doctrine\DBAL\ConnectionException
@@ -31,7 +32,7 @@ abstract class ProductMigration extends MigrationStep
 
         try {
             foreach ($products as $product) {
-                if(array_key_exists('name', $product)) {
+                if (array_key_exists('name', $product)) {
                     $name = $product['name'];
                     unset($product['name']);
                 }
@@ -44,40 +45,50 @@ abstract class ProductMigration extends MigrationStep
 
                 foreach ($languages as $locale => $language) {
                     $connection->insert(ProductTranslationDefinition::ENTITY_NAME, $this->quote($connection, [
-                        'name' => $name ?? $this->buildProductDescription($product, $locale),
-                        'description' => $this->buildProductDescription($product, $locale),
-                        'language_id' => $language['id'],
+                        'name'              => $name ?? $this->buildProductDescription($product, $locale),
+                        'description'       => $this->buildProductDescription($product, $locale),
+                        'language_id'       => $language['id'],
                         $productCodeIdField => $product['id'],
-                        'created_at' => $product['created_at'],
+                        'created_at'        => $product['created_at'],
                     ]));
                 }
             }
 
             $connection->commit();
-        } catch (\Exception $e) {
+        }
+        catch (\Exception $e) {
             $connection->rollBack();
             throw $e;
         }
     }
 
     /**
-     * @param Connection $connection
-     * @param array<string> $deleteIds
+     * @param Connection            $connection
+     * @param array<string, string> $deleteIds
      * @return void
      * @throws \Doctrine\DBAL\ConnectionException
      * @throws \Doctrine\DBAL\Exception
      */
-    public function deleteProducts(Connection $connection, array $deleteIds): void
+    public function deprecateProducts(Connection $connection, array $deleteIds): void
     {
         $connection->beginTransaction();
 
         try {
-            foreach ($deleteIds as $id) {
-                $connection->delete(ProductDefinition::ENTITY_NAME, ['id' => $id]);
+            foreach ($deleteIds as $oldId => $replaceId) {
+                $connection->update(
+                    'postnl_product',
+                    [
+                        'replaced_by_id' => Uuid::fromHexToBytes($replaceId),
+                    ],
+                    [
+                        'id' => Uuid::fromHexToBytes($oldId),
+                    ]
+                );
             }
 
             $connection->commit();
-        } catch (\Exception $e) {
+        }
+        catch (\Exception $e) {
             $connection->rollBack();
             throw $e;
         }
@@ -86,62 +97,62 @@ abstract class ProductMigration extends MigrationStep
 
     /**
      * @param array<mixed> $product
-     * @param string $locale
+     * @param string       $locale
      * @return string
      */
     private function buildProductDescription(array $product, string $locale): string
     {
         $translations = [
             'en-GB' => [
-                'destination_zone' => [
-                    Zone::EU => 'Parcel EU',
+                'destination_zone'                         => [
+                    Zone::EU     => 'Parcel EU',
                     Zone::GLOBAL => 'GlobalPack',
                 ],
-                'delivery_type' => [
+                'delivery_type'                            => [
                     DeliveryType::SHIPMENT => 'Standard Shipment',
-                    DeliveryType::PICKUP => 'Pick-up at PostNL location',
-                    DeliveryType::MAILBOX => 'Mailbox parcel',
+                    DeliveryType::PICKUP   => 'Pick-up at PostNL location',
+                    DeliveryType::MAILBOX  => 'Mailbox parcel',
                 ],
-                ProductDefinition::STOR_HOME_ALONE => 'Deliver to stated address only',
+                ProductDefinition::STOR_HOME_ALONE         => 'Deliver to stated address only',
                 ProductDefinition::STOR_RETURN_IF_NOT_HOME => 'Return when not home',
-                ProductDefinition::STOR_INSURANCE => 'Extra Cover',
-                ProductDefinition::STOR_SIGNATURE => 'Signature on delivery',
-                ProductDefinition::STOR_AGE_CHECK => 'Age check',
-                ProductDefinition::STOR_NOTIFICATION => 'Notification',
+                ProductDefinition::STOR_INSURANCE          => 'Extra Cover',
+                ProductDefinition::STOR_SIGNATURE          => 'Signature on delivery',
+                ProductDefinition::STOR_AGE_CHECK          => 'Age check',
+                ProductDefinition::STOR_NOTIFICATION       => 'Notification',
             ],
             'de-DE' => [
-                'destination_zone' => [
-                    Zone::EU => 'EU paket',
+                'destination_zone'                         => [
+                    Zone::EU     => 'EU paket',
                     Zone::GLOBAL => 'GlobalPack',
                 ],
-                'delivery_type' => [
+                'delivery_type'                            => [
                     DeliveryType::SHIPMENT => 'Standardversand',
-                    DeliveryType::PICKUP => 'Abholung am PostNL-Standort',
-                    DeliveryType::MAILBOX => 'Briefkasten-Paket',
+                    DeliveryType::PICKUP   => 'Abholung am PostNL-Standort',
+                    DeliveryType::MAILBOX  => 'Briefkasten-Paket',
                 ],
-                ProductDefinition::STOR_HOME_ALONE => 'Nur an die angegebene Adresse liefern',
+                ProductDefinition::STOR_HOME_ALONE         => 'Nur an die angegebene Adresse liefern',
                 ProductDefinition::STOR_RETURN_IF_NOT_HOME => 'Zurück, wenn nicht zu Hause sein',
-                ProductDefinition::STOR_INSURANCE => 'Zusätzliche Abdeckung',
-                ProductDefinition::STOR_SIGNATURE => 'Unterschrift bei Lieferung',
-                ProductDefinition::STOR_AGE_CHECK => 'Altersprüfung',
-                ProductDefinition::STOR_NOTIFICATION => 'Benachrichtigung',
+                ProductDefinition::STOR_INSURANCE          => 'Zusätzliche Abdeckung',
+                ProductDefinition::STOR_SIGNATURE          => 'Unterschrift bei Lieferung',
+                ProductDefinition::STOR_AGE_CHECK          => 'Altersprüfung',
+                ProductDefinition::STOR_NOTIFICATION       => 'Benachrichtigung',
             ],
             'nl-NL' => [
-                'destination_zone' => [
-                    Zone::EU => 'EU pakket',
+                'destination_zone'                         => [
+                    Zone::EU     => 'EU pakket',
                     Zone::GLOBAL => 'GlobalPack',
                 ],
-                'delivery_type' => [
+                'delivery_type'                            => [
                     DeliveryType::SHIPMENT => 'Standaard zending',
-                    DeliveryType::PICKUP => 'Ophalen bij een PostNL-punt',
-                    DeliveryType::MAILBOX => 'Brievenbuspakje',
+                    DeliveryType::PICKUP   => 'Ophalen bij een PostNL-punt',
+                    DeliveryType::MAILBOX  => 'Brievenbuspakje',
                 ],
-                ProductDefinition::STOR_HOME_ALONE => 'Alleen huisadres',
+                ProductDefinition::STOR_HOME_ALONE         => 'Alleen huisadres',
                 ProductDefinition::STOR_RETURN_IF_NOT_HOME => 'Retour bij geen gehoor',
-                ProductDefinition::STOR_INSURANCE => 'Verhoogd aansprakelijkheid',
-                ProductDefinition::STOR_SIGNATURE => 'Handtekening voor ontvangst',
-                ProductDefinition::STOR_AGE_CHECK => 'Leeftijdscontrole',
-                ProductDefinition::STOR_NOTIFICATION => 'Notificatie',
+                ProductDefinition::STOR_INSURANCE          => 'Verhoogd aansprakelijkheid',
+                ProductDefinition::STOR_SIGNATURE          => 'Handtekening voor ontvangst',
+                ProductDefinition::STOR_AGE_CHECK          => 'Leeftijdscontrole',
+                ProductDefinition::STOR_NOTIFICATION       => 'Notificatie',
             ],
         ];
 
@@ -152,7 +163,7 @@ abstract class ProductMigration extends MigrationStep
             $parts[] = $translations[$locale]['destination_zone'][$product['destination_zone']];
         }
 
-        foreach(array_keys(ProductDefinition::ALL_FLAGS) as $flag) {
+        foreach (array_keys(ProductDefinition::ALL_FLAGS) as $flag) {
             if (!!$product[$flag]) {
                 $parts[] = $translations[$locale][$flag];
             }
