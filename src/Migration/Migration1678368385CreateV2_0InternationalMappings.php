764ba87bca45bb26cb769f9108c150eb06bbb94c
Daniel Schipper <daniel@memo-ict.nl>
2023-03-13 11:36:46 +0100

    Update constants

--- src/Migration/Migration1678368385CreateV2_0InternationalMappings.php
+++ src/Migration/Migration1678368385CreateV2_0InternationalMappings.php
@@ -20,62 +20,62 @@ class Migration1678368385CreateV2_0InternationalMappings extends MappingMigratio
     {
         $this->insert($connection, ProductOptionRequiredMappingDefinition::ENTITY_NAME, [
             [
-                'product_id' => Defaults::PRODUCT_SHIPPING_NL_EU_4907,
+                'product_id' => Defaults::PRODUCT_SHIPPING_NL_EU_4907_005_025,
                 'option_id'  => Defaults::OPTION_101_012,
             ],
             [
-                'product_id' => Defaults::PRODUCT_SHIPPING_BE_EU_4907,
+                'product_id' => Defaults::PRODUCT_SHIPPING_BE_EU_4907_005_025,
                 'option_id'  => Defaults::OPTION_101_012,
             ],
         ]);
 
         $this->insert($connection, ProductOptionOptionalMappingDefinition::ENTITY_NAME, [
             [
-                'product_id' => Defaults::PRODUCT_SHIPPING_NL_EU_4907,
+                'product_id' => Defaults::PRODUCT_SHIPPING_NL_EU_4907_005_025,
                 'option_id'  => Defaults::OPTION_005_025,
             ],
             [
-                'product_id' => Defaults::PRODUCT_SHIPPING_NL_EU_4907,
+                'product_id' => Defaults::PRODUCT_SHIPPING_NL_EU_4907_005_025,
                 'option_id'  => Defaults::OPTION_004_015,
             ],
             [
-                'product_id' => Defaults::PRODUCT_SHIPPING_NL_EU_4907,
+                'product_id' => Defaults::PRODUCT_SHIPPING_NL_EU_4907_005_025,
                 'option_id'  => Defaults::OPTION_004_016,
             ],
             [
-                'product_id' => Defaults::PRODUCT_SHIPPING_BE_EU_4907,
+                'product_id' => Defaults::PRODUCT_SHIPPING_BE_EU_4907_005_025,
                 'option_id'  => Defaults::OPTION_005_025,
             ],
             [
-                'product_id' => Defaults::PRODUCT_SHIPPING_BE_EU_4907,
+                'product_id' => Defaults::PRODUCT_SHIPPING_BE_EU_4907_005_025,
                 'option_id'  => Defaults::OPTION_004_015,
             ],
             [
-                'product_id' => Defaults::PRODUCT_SHIPPING_BE_EU_4907,
+                'product_id' => Defaults::PRODUCT_SHIPPING_BE_EU_4907_005_025,
                 'option_id'  => Defaults::OPTION_004_016,
             ],
             [
-                'product_id' => Defaults::PRODUCT_SHIPPING_NL_GLOBAL_4909,
+                'product_id' => Defaults::PRODUCT_SHIPPING_NL_GLOBAL_4909_005_025,
                 'option_id'  => Defaults::OPTION_005_025,
             ],
             [
-                'product_id' => Defaults::PRODUCT_SHIPPING_NL_GLOBAL_4909,
+                'product_id' => Defaults::PRODUCT_SHIPPING_NL_GLOBAL_4909_005_025,
                 'option_id'  => Defaults::OPTION_004_015,
             ],
             [
-                'product_id' => Defaults::PRODUCT_SHIPPING_NL_GLOBAL_4909,
+                'product_id' => Defaults::PRODUCT_SHIPPING_NL_GLOBAL_4909_005_025,
                 'option_id'  => Defaults::OPTION_004_016,
             ],
             [
-                'product_id' => Defaults::PRODUCT_SHIPPING_BE_GLOBAL_4909,
+                'product_id' => Defaults::PRODUCT_SHIPPING_BE_GLOBAL_4909_005_025,
                 'option_id'  => Defaults::OPTION_005_025,
             ],
             [
-                'product_id' => Defaults::PRODUCT_SHIPPING_BE_GLOBAL_4909,
+                'product_id' => Defaults::PRODUCT_SHIPPING_BE_GLOBAL_4909_005_025,
                 'option_id'  => Defaults::OPTION_004_015,
             ],
             [
-                'product_id' => Defaults::PRODUCT_SHIPPING_BE_GLOBAL_4909,
+                'product_id' => Defaults::PRODUCT_SHIPPING_BE_GLOBAL_4909_005_025,
                 'option_id'  => Defaults::OPTION_004_016,
             ],
         ]);
--- src/Service/PostNL/Builder/ShipmentBuilder.php
+++ src/Service/PostNL/Builder/ShipmentBuilder.php
@@ -345,7 +345,7 @@ class ShipmentBuilder
             $productOptions[] = new ProductOption($requiredOption->getCharacteristic(), $requiredOption->getOption());
         }
 
-        if(in_array($product->getId(), [Defaults::PRODUCT_SHIPPING_NL_EU_4907, Defaults::PRODUCT_SHIPPING_BE_EU_4907])) {
+        if(in_array($product->getId(), [Defaults::PRODUCT_SHIPPING_NL_EU_4907_005_025, Defaults::PRODUCT_SHIPPING_BE_EU_4907_005_025])) {
             $tntOption = $product->getOptionalOptions()->filterByProperty('id', $config->getSendToEUTrackAndTrace())->first();
 
             if($tntOption instanceof OptionEntity) {
@@ -353,7 +353,7 @@ class ShipmentBuilder
             }
         }
 
-        if(in_array($product->getId(), [Defaults::PRODUCT_SHIPPING_NL_GLOBAL_4909, Defaults::PRODUCT_SHIPPING_BE_GLOBAL_4909])) {
+        if(in_array($product->getId(), [Defaults::PRODUCT_SHIPPING_NL_GLOBAL_4909_005_025, Defaults::PRODUCT_SHIPPING_BE_GLOBAL_4909_005_025])) {
             $tntOption = $product->getOptionalOptions()->filterByProperty('id', $config->getSendToWorldTrackAndTrace())->first();
 
             if($tntOption instanceof OptionEntity) {
