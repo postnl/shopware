b10c0d5516c1bf00befe8aba355ef0bebb0c47ce
Daniel Schipper <daniel@memo-ict.nl>
2023-05-10 16:59:49 +0200

    Add method to get correct config value

--- src/Service/PostNL/Product/DefaultProductService.php
+++ src/Service/PostNL/Product/DefaultProductService.php
@@ -5,14 +5,72 @@ declare(strict_types=1);
 namespace PostNL\Shopware6\Service\PostNL\Product;
 
 use PostNL\Shopware6\Defaults;
+use PostNL\Shopware6\Service\Shopware\ConfigService;
+use PostNL\Shopware6\Struct\Config\ProductSelectionStruct;
+use Psr\Log\LoggerInterface;
+use Shopware\Core\Framework\Context;
 
 class DefaultProductService
 {
+    protected ConfigService $configService;
+
+    protected LoggerInterface $logger;
+
+    public function __construct(
+        ConfigService   $configService,
+        LoggerInterface $logger
+    )
+    {
+        $this->configService = $configService;
+        $this->logger = $logger;
+    }
+
+    public function getConfigValue(
+        string  $sourceZone,
+        string  $destinationZone,
+        string  $deliveryType,
+        bool    $alternative,
+        Context $context,
+        ?string $salesChannelId = null
+    ): ProductSelectionStruct
+    {
+        $config = $this->configService->getConfiguration($salesChannelId, $context);
+
+        $method = implode('', [
+            'getProduct',
+            ucfirst(strtolower($deliveryType)),
+            ucfirst(strtolower($sourceZone)),
+            ucfirst(strtolower($destinationZone)),
+            ucfirst(strtolower($alternative ? 'alternative' : 'default')),
+        ]);
+
+        if(!method_exists($config, $method)) {
+            throw new \Exception(sprintf('Method %s does not exist in config', $method));
+        }
+
+        $struct = $config->{$method}();
+
+        if(!$struct instanceof ProductSelectionStruct) {
+            throw new \Exception('Unexpected config value');
+        }
+
+        return $struct;
+    }
+
+    /**
+     * @param string $sourceZone
+     * @param string $destinationZone
+     * @param string $deliveryType
+     * @return string
+     * @throws \Exception
+     */
     public function getFallback(
         string $sourceZone,
         string $destinationZone,
         string $deliveryType
-    ) {
+    )
+    {
+        /** @var array<string, string> $constants */
         $constants = Defaults::getConstants();
 
         $constant = strtoupper(sprintf(
