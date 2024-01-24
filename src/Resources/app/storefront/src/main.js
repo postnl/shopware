9dcc057997a1f9a723d9479bfecbe3be87467e20
Daniel Schipper <daniel@memo-ict.nl>
2024-01-24 11:51:14 +0100

    Remove old controlers and frontend plugins

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,9 +1,5 @@
 import PluginManager from 'src/plugin-system/plugin.manager';
 
-import PostnlPickupPointsPlugin from './plugin/postnl-pickup-points.plugin';
 import PostnlPostalCodeCheckPlugin from "./plugin/postnl-postal-code-check.plugin";
-import PostnlShippingDeliveryDatePlugin from "./plugin/postnl-shipping-delivery-date.plugin";
 
-PluginManager.register('PostnlPickupPoints', PostnlPickupPointsPlugin, '[data-postnl-pickup-points]');
 PluginManager.register('PostnlPostalCodeCheck', PostnlPostalCodeCheckPlugin, '[data-postnl-postal-code-check]');
-PluginManager.register('PostnlShippingDeliveryDate', PostnlShippingDeliveryDatePlugin, '[data-postnl-shipping-delivery-date]');
