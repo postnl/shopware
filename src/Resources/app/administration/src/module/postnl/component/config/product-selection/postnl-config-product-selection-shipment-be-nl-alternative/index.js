6530c7f5e938238c1b187b66b8f73cbd5814d9e3
Daniel Schipper <daniel@memo-ict.nl>
2024-02-20 11:46:56 +0100

    Add components for BE -> NL products

---
+++ src/Resources/app/administration/src/module/postnl/component/config/product-selection/postnl-config-product-selection-shipment-be-nl-alternative/index.js
@@ -0,0 +1,15 @@
+const { Component } = Shopware;
+
+Component.extend('postnl-config-product-selection-shipment-be-nl-alternative', 'postnl-config-product-selection-base', {
+
+    data() {
+        return {
+            sourceZone: 'BE',
+            destinationZone: 'NL',
+            deliveryType: 'shipment',
+
+            isAlt: true,
+            defaultIsEnabled: false,
+        };
+    },
+})
