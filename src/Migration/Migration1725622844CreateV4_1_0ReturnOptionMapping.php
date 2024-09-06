7ebfa7320b03d1b911fa5e8aba4aa583d53f961b
Daniel Schipper <daniel@memo-ict.nl>
2024-09-06 15:26:37 +0200

    Create new return options and mappings

--- src/Migration/Migration1725622844CreateV4_1_0ReturnOptionMapping.php
+++ src/Migration/Migration1725622844CreateV4_1_0ReturnOptionMapping.php
@@ -6,44 +6,46 @@ use Doctrine\DBAL\Connection;
 use PostNL\Shopware6\Component\Migration\MappingMigration;
 use PostNL\Shopware6\Defaults;
 use PostNL\Shopware6\Entity\Product\Aggregate\ProductOptionOptionalMappingDefinition;
+use Shopware\Core\Framework\Log\Package;
+use Shopware\Core\Framework\Migration\MigrationStep;
 
-class Migration1674219692CreateV2_0Mappings extends MappingMigration
+/**
+ * @internal
+ */
+#[Package('core')]
+class Migration1725622844CreateV4_1_0ReturnOptionMapping extends MappingMigration
 {
     public function getCreationTimestamp(): int
     {
-        return 1674219692;
+        return 1725622844;
     }
 
     public function update(Connection $connection): void
     {
         $productIds = [
             Defaults::PRODUCT_SHIPMENT_NL_NL_3085,
-            Defaults::PRODUCT_SHIPMENT_NL_NL_3087,
-            Defaults::PRODUCT_SHIPMENT_NL_NL_3089,
+            Defaults::PRODUCT_SHIPMENT_NL_NL_3385,
             Defaults::PRODUCT_SHIPMENT_NL_NL_3090,
+            Defaults::PRODUCT_SHIPMENT_NL_NL_3390,
+            Defaults::PRODUCT_SHIPMENT_NL_NL_3087,
             Defaults::PRODUCT_SHIPMENT_NL_NL_3094,
+            Defaults::PRODUCT_SHIPMENT_NL_NL_3089,
             Defaults::PRODUCT_SHIPMENT_NL_NL_3096,
             Defaults::PRODUCT_SHIPMENT_NL_NL_3189,
-            Defaults::PRODUCT_SHIPMENT_NL_NL_3385,
             Defaults::PRODUCT_SHIPMENT_NL_NL_3389,
-            Defaults::PRODUCT_SHIPMENT_NL_NL_3390,
-            Defaults::PRODUCT_SHIPMENT_NL_BE_4912,
-            Defaults::PRODUCT_SHIPMENT_NL_BE_4914,
-            Defaults::PRODUCT_SHIPMENT_NL_BE_4941,
-            Defaults::PRODUCT_SHIPMENT_NL_BE_4946,
+            Defaults::PRODUCT_SHIPMENT_NL_NL_3438,
         ];
 
         $optionsIds = [
-            Defaults::OPTION_118_006
+            Defaults::OPTION_152_025,
+            Defaults::OPTION_152_026,
+            Defaults::OPTION_152_028,
+            Defaults::OPTION_191_001,
+            Defaults::OPTION_191_004,
         ];
 
         $data = $this->createMatrix('product_id', $productIds, 'option_id', $optionsIds);
 
         $this->insert($connection, ProductOptionOptionalMappingDefinition::ENTITY_NAME, $data);
-    }
-
-    public function updateDestructive(Connection $connection): void
-    {
-        // implement update destructive
     }
 }
