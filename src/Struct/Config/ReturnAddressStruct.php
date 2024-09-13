3170b8700dc511655a26657b762be5d4e6c0631f
Daniel Schipper <daniel@memo-ict.nl>
2024-09-13 16:15:04 +0200

    Update return address component and struct with new switch

--- src/Struct/Config/ReturnAddressStruct.php
+++ src/Struct/Config/ReturnAddressStruct.php
@@ -4,139 +4,74 @@ namespace PostNL\Shopware6\Struct\Config;
 
 class ReturnAddressStruct extends ApiCompatibleStruct
 {
-    /**
-     * @var bool
-     */
-    protected $hasReturnContract;
+    protected bool $hasReturnContract;
+    protected bool $useHomeAddress;
+    protected string $companyName;
+    protected ?string $street;
+    protected ?string $houseNr;
+    protected ?string $houseNrExt;
+    protected string $zipcode;
+    protected string $city;
+    protected string $countrycode;
+    protected string $returnCustomerCode;
+    protected ?string $returnNumber;
 
-    /**
-     * @var string
-     */
-    protected $companyName;
-
-    /**
-     * @var string|null
-     */
-    protected $street;
-
-    /**
-     * @var string|null
-     */
-    protected $houseNr;
-
-    /**
-     * @var string|null
-     */
-    protected $houseNrExt;
-
-    /**
-     * @var string
-     */
-    protected $zipcode;
-
-    /**
-     * @var string
-     */
-    protected $city;
-
-    /**
-     * @var string
-     */
-    protected $countrycode;
-
-    /**
-     * @var string
-     */
-    protected $returnCustomerCode;
-
-    /**
-     * @var string|null
-     */
-    protected $returnNumber;
-
-    /**
-     * @return bool
-     */
     public function isHasReturnContract(): bool
     {
         return $this->hasReturnContract;
     }
 
-    /**
-     * @return string
-     */
+    public function isUseHomeAddress(): bool
+    {
+        return $this->useHomeAddress;
+    }
+
     public function getCompanyName(): string
     {
         return $this->companyName;
     }
 
-    /**
-     * @return string|null
-     */
     public function getStreet(): ?string
     {
-        if($this->getCountrycode() === 'NL' && $this->isHasReturnContract()) {
+        if($this->getCountrycode() === 'NL' && $this->isHasReturnContract() && !$this->isUseHomeAddress()) {
             return 'Antwoordnummer '. $this->getReturnNumber();
         }
 
         return $this->street;
     }
 
-    /**
-     * @return string|null
-     */
     public function getHouseNr(): ?string
     {
         return $this->houseNr;
     }
 
-    /**
-     * @return string|null
-     */
     public function getHouseNrExt(): ?string
     {
         return $this->houseNrExt;
     }
 
-    /**
-     * @return string
-     */
     public function getZipcode(): string
     {
         return $this->zipcode;
     }
 
-    /**
-     * @return string
-     */
     public function getCity(): string
     {
         return $this->city;
     }
 
-    /**
-     * @return string
-     */
     public function getCountrycode(): string
     {
         return $this->countrycode;
     }
 
-    /**
-     * @return string
-     */
     public function getReturnCustomerCode(): string
     {
         return $this->returnCustomerCode;
     }
 
-    /**
-     * @return string|null
-     */
     public function getReturnNumber(): ?string
     {
         return $this->returnNumber;
     }
-
-
 }
