bcb50aeff2210fd39e22cfcc3656a4f391da6206
Daniel Schipper <daniel@memo-ict.nl>
2024-09-13 17:35:01 +0200

    WIP added return options component

--- src/Resources/app/administration/src/module/postnl/component/config/index.js
+++ src/Resources/app/administration/src/module/postnl/component/config/index.js
@@ -4,5 +4,5 @@ import './postnl-required-hint';
 import './postnl-config-api-credentials-test';
 import './postnl-config-customer-data';
 import './postnl-config-sender-address';
-import './postnl-config-return-address';
+import './postnl-config-return-options';
 import './postnl-config-info';
