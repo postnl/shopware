83fb06b679516d57cd20f87f28d67cbefa53cd94
Daniel Schipper <daniel@memo-ict.nl>
2024-09-17 17:17:40 +0200

    Add base shipment modal

---
+++ src/Resources/app/administration/src/module/postnl/component/shipment-modal/shipment-modal-base/index.js
@@ -0,0 +1,93 @@
+import template from "shipment-modal-base.html.twig";
+
+const { Component, Mixin } = Shopware;
+
+Component.register('postnl-shipment-modal-base', {
+    template,
+
+    mixins: [
+        Mixin.getByName('notification'),
+    ],
+
+    props: {
+        selection: {
+            type: Object,
+            required: true,
+        }
+    },
+
+    data() {
+        return {
+            isLoading: false,
+            isProcessing: false,
+            isProcessingSuccess: false,
+        };
+    },
+
+    computed: {
+        modalClasses() {
+            return {}
+        },
+
+        modalTitle() {
+            return ''
+        },
+
+        isBulk() {
+            return this.selectionCount > 1;
+        },
+
+        isProcessingDisabled() {
+            return false
+        },
+
+        selectionCount() {
+            return Object.values(this.selection).length;
+        },
+
+        selectionHasProducts() {
+            return !Object.values(this.selection)
+                .map(order => order?.customFields?.postnl?.productId)
+                .some(productId => [undefined, null, ""].includes(productId))
+        },
+
+        orderIds() {
+            return Object.values(this.selection).map(order => order.id)
+        },
+    },
+
+    created() {
+        this.createdComponent();
+    },
+
+    watch: {
+        isSuccess(value) {
+            if (value === true) {
+                this.onProcessSuccess()
+                this.$emit('success-start')
+                return
+            }
+
+            this.onProcessSuccessEnd()
+            this.$emit('success-end')
+        }
+    },
+
+    methods: {
+        createdComponent() {
+        },
+
+        closeModal() {
+            if (!this.isProcessing) {
+                this.$emit('close');
+            }
+        },
+
+        onStartProcessing() {
+        },
+        onProcessSuccess() {
+        },
+        onProcessSuccessEnd() {
+        },
+    },
+})
