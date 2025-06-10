62038adce14d46ad81268a3dae0608bcd31605d3
Daniel Schipper <daniel@memo-ict.nl>
2025-06-10 16:28:44 +0200

    Move files to correct locations based on Shopware administration

--- src/Resources/app/administration/src/main.js
+++ src/Resources/app/administration/src/main.js
@@ -1,7 +1,6 @@
-import './app';
-import './core';
-import './init';
-import './mixin';
-import './module';
+// import './core';
+// import './init';
+// import './mixin';
+// import './module';
 
-import './decorator/rule-condition-service-decoration';
+// import './decorator/rule-condition-service-decoration';
