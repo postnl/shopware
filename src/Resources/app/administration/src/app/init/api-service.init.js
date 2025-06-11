7842d3c86dc320898ba137b8b4942add8bab0568
Daniel Schipper <daniel@memo-ict.nl>
2025-06-11 16:07:48 +0200

    Update api service loading

--- src/Resources/app/administration/src/app/init/api-service.init.js
+++ src/Resources/app/administration/src/app/init/api-service.init.js
@@ -1,24 +1,13 @@
-import ApiCredentialsService from '../core/service/api/api-credentials.service';
-import ProductSelectionService from '../core/service/api/product-selection.service';
-import ShipmentService from '../core/service/api/shipment.service';
+const services = import.meta.glob('./../../core/service/api/*.service.js', { eager: true, import: 'default' })
 
-// eslint-disable-next-line no-undef
-const { Application } = Shopware;
+Object
+    .values(services)
+    .forEach(serviceClass => {
+        const initContainer = Shopware.Application.getContainer('init')
+        const service = new serviceClass(initContainer.httpClient, Shopware.Service('loginService'))
 
-Application.addServiceProvider('PostNlApiCredentialsService', (container) => {
-    const initContainer = Application.getContainer('init');
+        const serviceName = service.name || serviceClass.name
 
-    return new ApiCredentialsService(initContainer.httpClient, container.loginService);
-});
-
-Application.addServiceProvider('ProductSelectionService', (container) => {
-    const initContainer = Application.getContainer('init');
-
-    return new ProductSelectionService(initContainer.httpClient, container.loginService);
-});
-
-Application.addServiceProvider('ShipmentService', (container) => {
-    const initContainer = Application.getContainer('init');
-
-    return new ShipmentService(initContainer.httpClient, container.loginService);
-});
+        Shopware.ApiService.register(serviceName, service)
+        Shopware.Application.addServiceProvider(serviceName, () => service)
+    })
