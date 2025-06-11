e26410a9f876c1d6fa855670f57851ff5ddad5c4
Daniel Schipper <daniel@memo-ict.nl>
2025-06-11 13:50:15 +0200

    Dynamically load components

--- src/Resources/app/administration/src/app/init/component.init.js
+++ src/Resources/app/administration/src/app/init/component.init.js
@@ -1,5 +1,31 @@
-const components = import.meta.glob('./../app/component/**/index.js')
+const components = import.meta.glob('./../component/**/index.js')
+import config from './../component/config.json'
 
 Object
     .entries(components)
-    .forEach(([path, importFn]) => Shopware.Component.register(path.split('/').slice(-2).shift(), importFn))
+    .forEach(([path, importFn]) => {
+        const componentName = path.split('/').slice(-2).shift()
+        const allowedTypes = ['extend', 'override']
+
+        if (config?.[componentName]) {
+            if (!'type' in config[componentName] || !allowedTypes.includes(config[componentName].type)) {
+                throw `Missing "type" field in ${ componentName } configuration. Must be one of ${ allowedTypes.join(', 0') }`
+            }
+
+            if (config[componentName].type === 'extend') {
+                if (!'component' in config[componentName]) {
+                    throw `Missing "component" field in ${ componentName } configuration.`
+                }
+
+                Shopware.Component.extend(componentName, config[componentName].component, importFn)
+            }
+
+            if (config[componentName].type === 'override') {
+                Shopware.Component.override(componentName, importFn)
+            }
+
+            return
+        }
+
+        Shopware.Component.register(componentName, importFn)
+    })
