dc6cdea19ed51dcc0db0f6e3e0212ef772b039a0
Daniel Schipper <daniel@memo-ict.nl>
2025-09-29 16:02:02 +0200

    Fix multiple calls for language id

--- src/Resources/app/administration/src/app/store/localeLanguage.store.js
+++ src/Resources/app/administration/src/app/store/localeLanguage.store.js
@@ -1,8 +1,11 @@
+const { Application, Context, Data: { Criteria }} = Shopware;
+
 export default {
     id: 'postnlLocaleLanguage',
 
     state: () => ({
         locales: {},
+        loadPromise: null,
     }),
 
     actions: {
@@ -13,5 +16,23 @@ export default {
 
             this.locales[locale] = languageId;
         },
+
+        loadLastKnownLanguage() {
+            const locale = Application.getContainer('factory').locale.getLastKnownLocale();
+
+            const criteria = new Criteria();
+            criteria.addFilter(Criteria.equals('locale.code', locale));
+
+            return this.loadPromise = Application
+                .getContainer('service')
+                .repositoryFactory
+                .create('language')
+                .searchIds(criteria, Context.api)
+                .then(result => {
+                    const languageId = result.data[0];
+                    this.addLanguage(locale, languageId);
+                    return languageId;
+                })
+        }
     },
 };
